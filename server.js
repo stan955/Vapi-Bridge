a61fb8a9f0193cb133135397b9d3c16851039cd2
stan955 <stan@themapdoctors.com>
2026-01-29 16:44:20 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("INCOMING:", req.method, req.url);
+  next();
+});
+
+
 // Health check (Render will hit this)
 app.get("/", (req, res) => {
   res.send("alive");
